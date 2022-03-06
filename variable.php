634c5a9915e35cbf9d8fbbb9bc4c454555b03d3d
deveuni <vividdream0215@naver.com>
2022-03-06 16:35:28 +0900

    변수사용법

---
+++ variable.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <body>
+    <h1>Variable</h1>
+    <?php
+    $name = "egoing";
+     echo "Lorem ipsum dolor sit amet, consectetur ".$name." adipisicing elit,
+     sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+     Ut enim ad minim veniam, quis nostrud ".$name." exercitation ullamco laboris
+     nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+     in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+     occaecat ".$name." cupidatat non proident, sunt in culpa qui officia deserunt
+     ".$name." mollit anim id est laborum.
+    ";
+    ?>
+  </body>
+</html>
